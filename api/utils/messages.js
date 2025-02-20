fb8e9144ff91316bc46a9d1ca93b68c4e7043aa9
Muhammad Niazi <github.muhammadniazi@outlook.com>
2025-02-20 14:58:03 +0500

    Create dedicated messages file

---
+++ api/utils/messages.js
@@ -0,0 +1,9 @@
+module.exports = {
+    auth: {
+        connectionAuthenticated: 'Connection authenticated successfully',
+    },
+    users: {
+        allUsersRetrieved: 'All users retrieved successfully',
+        failedToRetrieveAllUsers: 'Failed to retrieve all users',
+    },
+};
