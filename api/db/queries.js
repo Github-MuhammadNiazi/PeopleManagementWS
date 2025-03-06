979e69819958384bf808f1c6a90cf79190e2ba66
Muhammad Niazi <github.muhammadniazi@outlook.com>
2025-03-06 15:34:57 +0500

    Added new db query
    Added query to fetch login

--- api/db/queries.js
+++ api/db/queries.js
@@ -1,5 +1,10 @@
+var packageJson = require('../package.json');
+
 module.exports = {
     userRoles: {
-        getAllUserRoles: 'SELECT * FROM public."UserRoles"',
+        getAllUserRoles: `SELECT * FROM ${packageJson.dbSchema}."UserRoles"`,
     },
+    systemUsers: {
+        login: `SELECT * FROM ${packageJson.dbSchema}."SystemUsers" WHERE "Username" = {{username}} AND "Password" = {{password}}`,
+    }
 };
