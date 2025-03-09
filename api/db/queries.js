170daaac9ef372c03ee3afdeefadb78dbce556c9
Muhammad Niazi <github.muhammadniazi@outlook.com>
2025-03-09 23:27:39 +0500

    Added queries for user sign up

--- api/db/queries.js
+++ api/db/queries.js
@@ -1,10 +1,21 @@
 var constants = require('../utils/constants');
 
 module.exports = {
+    dbTransactions: {
+        begin: `BEGIN`,
+        commit: `COMMIT`,
+        rollback: `ROLLBACK`,
+    },
     userRoles: {
         getAllUserRoles: `SELECT * FROM ${constants.defaultConfigurations.dbSchema}."UserRoles"`,
+        getUserRoleById: `SELECT * FROM ${constants.defaultConfigurations.dbSchema}."UserRoles" WHERE "UserRoleId" = $1`,
     },
     systemUsers: {
-        login: `SELECT * FROM ${constants.defaultConfigurations.dbSchema}."SystemUsers" WHERE "Username" = $1 AND "Password" = $2`,
+        createSystemUser: `INSERT INTO ${constants.defaultConfigurations.dbSchema}."SystemUsers" ("UserId", "UserRoleId", "Username", "Password") VALUES ($1, $2, $3, $4) RETURNING *`,
+        getUserByUsername: `SELECT * FROM ${constants.defaultConfigurations.dbSchema}."SystemUsers" WHERE "Username" = $1`,
+    },
+    Users: {
+        createUser: `INSERT INTO ${constants.defaultConfigurations.dbSchema}."Users" ("FirstName", "LastName", "IdentificationNumber", "ContactNumber", "Email", "IsApartment", "Apartment", "Building", "Street", "Region", "City", "Country", "IsForeigner") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *`,
+        getAllUsers: `SELECT * FROM ${constants.defaultConfigurations.dbSchema}."Users"`,
     }
 };
