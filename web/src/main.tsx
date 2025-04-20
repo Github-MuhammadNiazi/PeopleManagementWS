439cc754c9e1d8de311d208d29b5a3aa3d529a0f
Muhammad Niazi <github.muhammadniazi@outlook.com>
2025-04-20 16:35:04 +0500

    Created Login page layout

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,3 +1,7 @@
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css';
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -7,9 +11,7 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <App />
-      </div>
     </BrowserRouter>
   </StrictMode>,
 )
