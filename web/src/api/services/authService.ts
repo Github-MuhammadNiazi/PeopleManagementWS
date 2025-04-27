db1290ffbcdf24a2f4d653890efa95087f953897
Muhammad Niazi <github.muhammadniazi@outlook.com>
2025-04-27 17:23:03 +0500

    Added API service

--- web/src/api/services/authService.ts
+++ web/src/api/services/authService.ts
@@ -1,8 +1,10 @@
 import { apiClient } from '../config/apiConfig';
+import * as endpoints from '../config/endpoints';
+import { LoginRequest, LoginResponse } from '../types/apiTypes';
 
-const authenticateConnection = async () => {
+const authenticateConnection = async (): Promise<void> => {
     try {
-        const response = await apiClient.get('/auth');
+        const response = await apiClient.get(endpoints.auth.authenticateConnection);
         return response.data;
     } catch (error) {
         console.error('Error in authenticateConnection:', error);
@@ -10,4 +12,17 @@ const authenticateConnection = async () => {
     }
 };
 
-export { authenticateConnection };
+const login = async (requestBody: LoginRequest): Promise<LoginResponse> => {
+    try {
+        const response = await apiClient.post<LoginResponse>(endpoints.auth.login, requestBody);
+        return response.data;
+    } catch (error) {
+        console.error('Error in login:', error);
+        throw error;
+    }
+};
+
+export {
+    authenticateConnection,
+    login,
+};
